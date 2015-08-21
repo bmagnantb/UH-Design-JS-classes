69bd97482900480957fc9b00fb57adf67d8dbcf9
bmagnantb <bmagnantb@gmail.com>
2015-08-21 13:04:50 -0500

    refactored Slide and SlideView, added Slide styles

--- src/js/app/components/Slide.jsx
+++ src/js/app/components/Slide.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import marked from 'marked'
+import highlight from 'highlight.js'
 
-require('./Slide.scss')
+import './Slide.scss'
+import 'highlight.js/styles/agate.css'
+
+marked.setOptions({
+	gfm: true,
+	highlight: code => highlight.highlight('javascript', code).value
+})
 
 export default class Slide {
 	render() {
 		return (
-			<div>
-				<h1>{this.props.title}</h1>
-				<p>{this.props.content}</p>
+			<div className="slide">
+				<div className="slide-titles">
+					<h1>{this.props.title}</h1>
+					<h3>{this.props.subtitle}</h3>
+				</div>
+				<div className="slide-content" 
+					dangerouslySetInnerHTML={this.makeContent(this.props.content)} />
 			</div>
 		)
+	}
+
+	makeContent(content) {
+		return {__html: marked(content)}
 	}
 }
--- src/js/app/views/SlideView.jsx
+++ src/js/app/views/SlideView.jsx
@@ -1,16 +1,53 @@
 import _ from 'lodash'
+import Rx from 'rx'
 import React from 'react'
 import { Link } from 'react-router'
 import 	{ connect } from 'react-redux'
 
+import { getCamelCaseLessonName } from '../../utils'
 import { Slide } from '../components'
 
-require('./SlideView.scss')
+import './SlideView.scss'
+
+const LEFT_ARROW = 37
+const RIGHT_ARROW = 39
 
 class SlideView {
+	componentDidMount() {
+		var sideArrowKeyup$ = Rx.Observable.fromEvent(window, 'keyup')
+			.filter(evt => evt.keyCode === LEFT_ARROW || evt.keyCode === RIGHT_ARROW)
+			.map(evt => evt.keyCode === LEFT_ARROW ? -1 : 1)
+			.subscribe(indexChange => {
+				var slideIndex = this.getSlideIndex() + indexChange
+				if (this.props.slides[slideIndex]) {
+					var name = this.getLessonName()
+					this.context.router.transitionTo(`/slides/${name}/${slideIndex + 1}`)
+				} else 
+					return // TODO something to signify beginning of slideshow
+			})
+		
+		this.handlers = {
+			sideArrowKeyup$
+		}
+	}
+	
+	componentWillUnmount() {
+		var { sideArrowKeyup$ } = this.handlers
+		sideArrowKeyup$.dispose()
+	}
+	
 	render() {
-		if (_.isEmpty(this.props.slide)) this.context.router.replaceWith('/slides')
-		return <Slide {...this.props.slide} />
+		var slide = this.props.slides[this.getSlideIndex()]
+		if (_.isEmpty(slide)) this.context.router.replaceWith('/slides')
+		return <Slide {...slide} />
+	}
+	
+	getLessonName() {
+		return getCamelCaseLessonName(this.props.params.name)
+	}
+	
+	getSlideIndex() {
+		return Number.parseInt(this.props.params.number || 1) - 1
 	}
 }
 
@@ -19,7 +56,6 @@ SlideView.contextTypes = {
 }
 
 export default connect((state, props) => {
-	var name = props.params.name.replace(/-([a-z])/g, (match, group) => group.toUpperCase())
-	var number = Number.parseInt(props.params.number) - 1 || 0
-	return {slide: state[name] && state[name].slides[number] || {}}
+	var name = getCamelCaseLessonName(props.params.name)
+	return {slides: state[name].slides || {}}
 })(SlideView)
