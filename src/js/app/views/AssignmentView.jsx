6da2006353cb991cfb7889aa97c9e795694412b0
bmagnantb <bmagnantb@gmail.com>
2015-08-22 19:07:02 -0500

    added markdown for AssignmentView content, moved redux select anon function to defined function for export clarity

--- src/js/app/views/AssignmentView.jsx
+++ src/js/app/views/AssignmentView.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
+import marked from 'marked'
+import highlight from 'highlight.js'
 
 import { getCamelCaseLessonName } from '../../utils'
 
 import './AssignmentView.scss'
 
+marked.setOptions({
+	gfm: true,
+	highlight: code => highlight.highlight('javascript', code).value,
+	langPrefix: ''
+})
+
 class AssignmentView {
 	render() {
 		return (
-			<div>
+			<div className="assignment">
 				<h3><Link to={`/assignments/${this.props.params.name}`}>Return to {this.props.lessonName}</Link></h3>
 				<h2>{this.props.assignment.title}</h2>
-				<p>{this.props.assignment.content}</p>
+				<div dangerouslySetInnerHTML={this.getContent(this.props.assignment.content)} />
 			</div>
 		)
 	}
+
+	getContent(content) {
+		return {__html: marked(content)}
+	}
 }
 
-export default connect((state, props) => {
+export default connect(select)(AssignmentView)
+
+function select(state, props) {
 	var name = getCamelCaseLessonName(props.params.name)
 	return {
 		lessonName: state[name].name,
 		assignment: state[name].assignments.filter(assignment => assignment.route === props.params.assignment)[0]
 	}
-})(AssignmentView)
+}
