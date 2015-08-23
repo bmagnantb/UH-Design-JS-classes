787abd44abf97db61969bafd5a1bf993ddacbd5e
bmagnantb <bmagnantb@gmail.com>
2015-08-23 17:14:13 -0500

    connected LessonListView to redux

--- src/js/app/views/LessonListView.jsx
+++ src/js/app/views/LessonListView.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { connect } from 'react-redux'
 
-import './LessonListView'
+import { makeLessonList } from '../../utils'
 
-export default class LessonListView {
+import './LessonListView.scss'
+
+class LessonListView {
 	render() {
-		return <h3>Lesson List</h3>
+		var lessons = this.props.lessons
+			.map(({ key, lesson }) => {
+				console.log(lesson)
+				return (
+					<li>
+						<h3><Link to={`/lesson/${key}`}>{lesson.name}</Link></h3>
+						<ul>
+							{lesson.slides ? <li><Link to={`/slides/${key}`}>Slides</Link></li> : null}
+							<li><Link to={`/assignments/${key}`}>Assignments</Link></li>
+						</ul>
+					</li>
+				)
+			})
+
+		return (
+			<div className="lesson-list">
+				<h1>Lessons</h1>
+				<ul>
+					{lessons}
+				</ul>
+			</div>
+		)
 	}
+}
+
+export default connect(select)(LessonListView)
+
+function select(state, props) {
+	return { lessons: makeLessonList(state) }
 }
