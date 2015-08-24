e42e5b8df4a17414e7ab005830023ae3ddcf2fff
bmagnantb <bmagnantb@gmail.com>
2015-08-24 09:50:01 -0500

    simplified LessonListView render, uses lessonHas utils

--- src/js/app/views/LessonListView.jsx
+++ src/js/app/views/LessonListView.jsx
@@ -2,23 +2,20 @@ import React from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 
-import { makeLessonList } from '../../utils'
+import {
+	getHyphenatedLessonName,
+	lessonHasAssignments,
+	lessonHasSlides,
+	makeLessonList
+} from '../../utils'
 
 import './LessonListView.scss'
 
 class LessonListView {
 	render() {
 		var lessons = this.props.lessons
-			.map(({ key, lesson }) => {
-				var route = (lesson.slides && lesson.assignments)
-					? `/lesson/${key}`
-					: lesson.slides ? `/slides/${key}` : `/assignments/${key}`
-				return (
-					<li key={`lessonlist-lesson-${key}`}>
-						<h3><Link to={route}>{lesson.name}</Link></h3>
-					</li>
-				)
-			})
+			.filter(lesson => lessonHasAssignments(lesson) || lessonHasSlides(lesson))
+			.map(makeMarkup)
 
 		return (
 			<div className="lessonlist-lesson-list">
@@ -35,4 +32,20 @@ export default connect(select)(LessonListView)
 
 function select(state, props) {
 	return { lessons: makeLessonList(state) }
+}
+
+function makeMarkup({ key, lesson }) {
+	var route
+	var routeKey = getHyphenatedLessonName(key)
+	var hasAssignments = lessonHasAssignments({lesson})
+	var hasSlides = lessonHasSlides({lesson})
+	if (hasAssignments && hasSlides) route = `/lesson/${routeKey}`
+	else if (hasAssignments) route = `/assignments/${routeKey}`
+	else if (hasSlides) route = `/slides/${routeKey}`
+
+	return (
+		<li key={`lessonlist-lesson-${key}`}>
+			<h3><Link to={route}>{lesson.name}</Link></h3>
+		</li>
+	)
 }
