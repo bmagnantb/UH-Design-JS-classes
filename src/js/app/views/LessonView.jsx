247d1da70ea467d89395398b41d01d71fde0ba54
bmagnantb <bmagnantb@gmail.com>
2015-08-24 09:32:26 -0500

    fixed select function to translate route name, links to assignment page if only one assignment for lesson

--- src/js/app/views/LessonView.jsx
+++ src/js/app/views/LessonView.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 
-import { getHyphenatedLessonName, lessonHasAssignments, lessonHasSlides } from '../../utils'
+import {
+	getHyphenatedLessonName,
+	getCamelCaseLessonName,
+	lessonHasAssignments,
+	lessonHasSlides
+} from '../../utils'
 
 import './LessonView.scss'
 
 class LessonView {
 	render() {
 		var { lesson } = this.props
-		var routeName = getHyphenatedLessonName(this.props.params.name)
+		var routeName = this.props.params.name
+		var assignmentNum = lessonHasAssignments({lesson})
 
 		return (
 			<div>
 				<h1>{lesson.name}</h1>
-				{lessonHasAssignments({lesson})
-					? <h3><Link to={`/assignments/${routeName}`}>Assignments</Link></h3>
-					: null}
+				{!assignmentNum
+					? null
+					: assignmentNum > 1
+						? <h3><Link to={`/assignments/${routeName}`}>Assignments</Link></h3>
+						: <h3><Link to={`/assignments/${routeName}/${lesson.assignments[0].route}`}>Assignment</Link></h3>}
 				{lessonHasSlides({lesson})
 					? <h3><Link to={`/slides/${routeName}`}>Slides</Link></h3>
 					: null}
@@ -31,7 +39,7 @@ class LessonView {
 export default connect(select)(LessonView)
 
 function select(state, props) {
-	return { lesson: state[props.params.name] }
+	return { lesson: state[getCamelCaseLessonName(props.params.name)] }
 }
 
 function makeOutline(outline, routeName) {
