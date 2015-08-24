dba482fba539f146332bd1db375aeddfb8ad4ffa
bmagnantb <bmagnantb@gmail.com>
2015-08-24 09:14:41 -0500

    modified link to return to lesson page if only 1 assignment for lesson

--- src/js/app/views/AssignmentView.jsx
+++ src/js/app/views/AssignmentView.jsx
@@ -8,9 +8,13 @@ import './AssignmentView.scss'
 
 class AssignmentView {
 	render() {
+		var route = this.props.assignmentTotal > 1
+			? `/assignments/${this.props.params.name}`
+			: `/lesson/${this.props.params.name}`
+
 		return (
 			<div className="assignment">
-				<h3><Link to={`/assignments/${this.props.params.name}`}>Return to {this.props.lessonName}</Link></h3>
+				<h3><Link to={route}>{this.props.lessonName}</Link></h3>
 				<h2>{this.props.assignment.title}</h2>
 				<div dangerouslySetInnerHTML={convertMarkdown(this.props.assignment.content)} />
 			</div>
@@ -24,6 +28,7 @@ function select(state, props) {
 	var name = getCamelCaseLessonName(props.params.name)
 	return {
 		lessonName: state[name].name,
+		assignmentTotal: state[name].assignments.length,
 		assignment: state[name].assignments.filter(assignment => assignment.route === props.params.assignment)[0]
 	}
 }
