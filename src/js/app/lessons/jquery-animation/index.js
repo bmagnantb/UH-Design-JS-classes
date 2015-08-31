3695126e838b2df509d8ae6ad51048aef84f2a7e
bmagnantb <bmagnantb@gmail.com>
2015-08-31 00:15:19 -0500

    added outline for jquery-animation lesson

--- src/js/app/lessons/jquery-animation/index.js
+++ src/js/app/lessons/jquery-animation/index.js
@@ -1,5 +1,6 @@
 import slides from './slides'
 import assignments from './assignments'
+import outline from './outline'
 
 var name = 'jQuery Animation'
 var date = Date.parse('Sep 1 2015')
@@ -8,5 +9,6 @@ export default {
 	slides,
 	assignments,
 	name,
-	date
+	date,
+	outline
 }
---
+++ src/js/app/lessons/jquery-animation/outline.js
@@ -0,0 +1,34 @@
+export default [
+	{
+		text: 'JS Key Points',
+		slideNum: 2
+	},
+	{
+		text: 'Using JS in Web Pages',
+		slideNum: 6
+	},
+	{
+		text: 'The DOM',
+		slideNum: 10
+	},
+	{
+		text: 'about jQuery',
+		slideNum: 12
+	},
+	{
+		text: '$',
+		slideNum: 13
+	},
+	{
+		text: 'Built-in helpers',
+		slideNum: 15
+	},
+	{
+		text: 'Built-in Animations',
+		slideNum: 19
+	},
+	{
+		text: '.animate()',
+		slideNum: 23
+	}
+]
