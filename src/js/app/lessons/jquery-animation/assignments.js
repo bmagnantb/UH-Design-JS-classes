a44e64d95e3746642d04e5a5e13302f19518fff1
bmagnantb <bmagnantb@gmail.com>
2015-08-31 22:17:40 -0500

    added assignment for jquery-animation lesson

--- src/js/app/lessons/jquery-animation/assignments.js
+++ src/js/app/lessons/jquery-animation/assignments.js
@@ -1,2 +1,13 @@
 export default [
+	{
+		title: 'Site Intro Animations',
+		route: 'intro-animations',
+		content: `
+1. Download these files, one possible solution for the html/css prework, so we are all working on the same html and css:
+	+ <a href="build/downloads/jquery-animation/assignment/index.html" download>HTML</a>
+	+ <a href="build/downloads/jquery-animation/assignment/style.css" download>CSS</a>
+
+2. Add some animations that happen when the page loads.
+`
+	}
 ]
