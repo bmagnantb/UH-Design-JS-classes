9418f532cae77bf3da5f7612c82272e84565ca8b
bmagnantb <bmagnantb@gmail.com>
2015-09-17 09:42:54 -0500

    cancelled dom manipulation lesson

--- src/js/app/reducers/index.js
+++ src/js/app/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux'
 import prework from './prework'
 import introJs from './intro-js'
 import jqueryAnimation from './jquery-animation'
-import jqueryDomManipulation from './jquery-dom-manipulation'
 import jqueryEvents from './jquery-events'
 import recap from './recap'
 
@@ -11,7 +10,6 @@ export default combineReducers({
 	prework,
 	introJs,
 	jqueryAnimation,
-	jqueryDomManipulation,
 	jqueryEvents,
 	recap
 })
--- src/js/app/reducers/jquery-dom-manipulation.js
+++
@@ -1,5 +0,0 @@
-import initial from '../lessons/jquery-dom-manipulation'
-
-export default function(state = initial, action) {
-	return initial
-}
